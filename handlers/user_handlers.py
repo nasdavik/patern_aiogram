ac8eb8f58de42210647f28eadaf65fbd79b698b9
nasdavik <120183618+nasdavik@users.noreply.github.com>
2023-06-04 18:21:22 +0500

    Add buttons and new packedges

--- handlers/user_handlers.py
+++ handlers/user_handlers.py
@@ -1,19 +1,35 @@
 from aiogram import Router
-from aiogram.filters import Command, CommandStart
-from aiogram.types import Message
+from aiogram.filters import Command, CommandStart, Text
 from lexicon.lexicon import LEXICON_RU
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, Message
+from aiogram.utils.keyboard import ReplyKeyboardBuilder
 
 # Инициализируем роутер уровня модуля
 router: Router = Router()
+
+# Добавим кнопки
+btns = [KeyboardButton(text=f"{x}") for x in ("Камень", "Ножницы", "Бумага")]
+
+# Создаем билдер с кнопками
+keyboard: ReplyKeyboardBuilder = ReplyKeyboardBuilder()
+
+keyboard.row(*btns, width=3)
+
+# Создаем объект клавиатуры, добавляя в него кнопки
+my_keyboard: ReplyKeyboardMarkup = keyboard.as_markup(
+    resize_keyboard=True)
 
 
 # Этот хэндлер срабатывает на команду /start
 @router.message(CommandStart())
 async def process_start_command(message: Message):
-    await message.answer(text=LEXICON_RU['/start'])
+    await message.answer(text=LEXICON_RU['/start'],
+                         reply_markup=my_keyboard)
 
 
 # Этот хэндлер срабатывает на команду /help
 @router.message(Command(commands='help'))
 async def process_help_command(message: Message):
     await message.answer(text=LEXICON_RU['/help'])
+
+
--- main.py
+++ main.py
@@ -1,17 +1,30 @@
 import asyncio
+import logging
 
 from aiogram import Bot, Dispatcher
 from config_data.config import Config, load_config
 from handlers import other_handlers, user_handlers
 
+# Иницилизируем логгер
+logger = logging.getLogger(__name__)
 
 # Функция конфигурирования и запуска бота
 async def main():
+    # Конфигурируем логирование
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(filename)s:%(lineno)d #%(levelname)-8s '
+               '[%(asctime)s] - %(name)s - %(message)s')
+
+    # Выводим в консоль информацию о начале запуска бота
+    logger.info('Starting bot')
+
     # Загружаем конфиг в переменную config
     config: Config = load_config()
 
     # Инициализируем бот и диспетчер
-    bot: Bot = Bot(token=config.tg_bot.token)
+    bot: Bot = Bot(token=config.tg_bot.token,
+                   parse_mode="HTML")
     dp: Dispatcher = Dispatcher()
 
     # Регистриуем роутеры в диспетчере
