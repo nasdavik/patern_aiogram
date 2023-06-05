749a09ab07af746f0ae27d588d33344054af2277
nasdavik <120183618+nasdavik@users.noreply.github.com>
2023-06-05 22:05:37 +0500

    Created Menu

--- lexicon/lexicon.py
+++ lexicon/lexicon.py
@@ -18,3 +18,8 @@ LEXICON_RU: dict[str, str] = {
     'user_won': 'Ты победил! Поздравляю!\n\nДавай сыграем еще?',
     'nobody_won': 'Ничья!\n\nПродолжим?',
     'bot_choice': 'Мой выбор'}
+
+LEXICON_COMMANDS_RU: dict[str, str] = {
+    '/start': 'Стартуем',
+    '/help': 'Помощь'
+}
--- main.py
+++ main.py
@@ -4,9 +4,11 @@ import logging
 from aiogram import Bot, Dispatcher
 from config_data.config import Config, load_config
 from handlers import other_handlers, user_handlers
+from keyboards.set_menu import set_main_menu
 
 # Иницилизируем логгер
 logger = logging.getLogger(__name__)
+
 
 # Функция конфигурирования и запуска бота
 async def main():
@@ -26,6 +28,10 @@ async def main():
     bot: Bot = Bot(token=config.tg_bot.token,
                    parse_mode="HTML")
     dp: Dispatcher = Dispatcher()
+
+    # Регистрируем асинхронную функцию в диспетчере,
+    # которая будет выполняться на старте бота,
+    await set_main_menu(bot)
 
     # Регистриуем роутеры в диспетчере
     dp.include_router(user_handlers.router)
