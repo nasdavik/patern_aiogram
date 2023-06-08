a4bef80491bc826bfbb3234b3783cc40dc861934
nasdavik <sew542@mail.ru>
2023-06-08 18:38:25 +0500

    Tryed ending _get_part_text

--- services/file_handling.py
+++ services/file_handling.py
@@ -8,13 +8,26 @@ book: dict[int, str] = {}
 
 # Функция, возвращающая строку с текстом страницы и ее размер
 def _get_part_text(text: str, start: int, size: int) -> tuple[str, int]:
-    pass
+    marks = '...,;:?!'
+    if text[:size][-1] in marks and text[:size+1][-1] not in marks:
+        answer = ''.join(text[start:start+size+1])
+        count = len(answer.strip())
+        return (answer,count)
+    else:
+        while text[:start + size][-1] not in marks or text[:size+1][-1] in marks:
+            size -= 1
+        answer = ''.join(text[start:start + size])
+        count = len(answer.strip())
+        return (answer,count)
 
 
+with open("test.txt", encoding="utf8") as r:
+    text = r.read()
+    print(*_get_part_text(text, 5, 9), sep='\n')
 # Функция, формирующая словарь книги
 def prepare_book(path: str) -> None:
     pass
 
 
 # Вызов функции prepare_book для подготовки книги из текстового файла
-prepare_book(os.path.join(os.getcwd(), BOOK_PATH))git
+# prepare_book(os.path.join(os.getcwd(), BOOK_PATH))
